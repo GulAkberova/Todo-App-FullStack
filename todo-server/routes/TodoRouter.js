9c510f967910aaf419af0db7f3455fcb702b105c
Gul Akberova <kbrovagul0@gmail.com>
2023-02-03 00:28:34 +0400

    first commit

---
+++ todo-server/routes/TodoRouter.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const { todoController } = require('../controller/TodoController')
+
+const router=express.Router()
+
+router.get('/',todoController.getAll)
+router.post('/',todoController.getPost)
+router.get('/:id',todoController.getById)
+router.delete('/:id',todoController.getDelete)
+router.put('/:id',todoController.getPost)
+
+
+module.exports=router
+
